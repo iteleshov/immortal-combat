d75503960ce904f4a3e9a31debca47a766da913b
iteleshov <egervantu@gmail.com>
2025-10-28 22:37:14 +0500

    fix cors

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from backend.services.knowledge_facade import KnowledgeBaseFacade
 from backend.models.gene_response import GeneResponse
 
 app = FastAPI(title="Longevity Gene Knowledge API (UniProt + NCBI)")
+
+origins = [
+    "https://www.gene-lens.site",
+    "https://gene-lens.site",
+]
+
+# ✅ Подключаем CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 facade = KnowledgeBaseFacade()
 
 @app.get('/search', response_model=GeneResponse)
